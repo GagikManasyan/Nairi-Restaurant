954c5cd8cd35300e17c81248a410de5a392c77ea
GagikManasyan <gagik_manasyan@edu.aua.am>
2023-09-18 21:58:41 +0400

    venue slider

--- src/about.jsx
+++ src/about.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./css/about.css";
 
 function About() {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const images = [
+    "venue1.jpg",
+    "venue1.jpg",
+    "venue3.jpg",
+    "venue4.jpg",
+    "venue5.jpg",
+  ];
+  useEffect(() => {
+    const preloadImages = () => {
+      images.forEach((image) => {
+        const img = new Image();
+        img.src = `/images/venue/${image}`;
+      });
+    };
+    preloadImages();
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const backgroundImageStyle = {
+    backgroundImage: `url('/images/venue/${images[currentImageIndex]}')`,
+    transition: "background-image 1s ease-in-out",
+    loading: "lazy",
+  };
   return (
     <div className="section" id="about">
       <div className="welcome">
@@ -20,7 +52,7 @@ function About() {
         </p>
       </div>
       <div className="venue">
-        <div className="venue-background"></div>
+        <div className="venue-background" style={backgroundImageStyle}></div>
         <div className="venue-info">
           <span className="org-text">
             Our venue is the perfect place for special events.
